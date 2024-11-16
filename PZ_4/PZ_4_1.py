050b246a407eed4ce2b30f878ea073c7fe21f044
dioloxmadarasolo <tkeyan@inbox.ru>
2024-11-16 16:28:20 +0300

    закончил пз5

--- PZ_4/PZ_4_1.py
+++ PZ_4/PZ_4_1.py
@@ -1,13 +1,20 @@
 # Дано целое число N (>0). Найти значение выражения 1.1 - 1.2 + 1.3 - ... (N слагаемых, знаки чередуются). Условный оператор не использовать.
+try:
+    N = int(input("Введите число больше нуля: "))
 
-N = int(input("Введите число больше нуля: "))
+except ValueError:
+   print("Введите число!")
+
 summ = 0
 pm = True
-for i in range(1, N + 1):
-    if pm:
-        summ += 1 / i
-    else:
-        summ -= 1 / i
-    pm = not pm
 
-print(summ)
+if N > 0:
+    for i in range(1, N + 1):
+        if pm:
+            summ += 1 / i
+        else:
+            summ -= 1 / i
+        pm = not pm
+        print(summ)
+else:
+    print("N должно быть больше 0!")
