55a5e00bf9f0d51dee380f0f4025923a0678b8e0
dioloxmadarasolo <tkeyan@inbox.ru>
2025-01-25 16:06:28 +0300

    PZ 9

--- PZ_9/PZ_9.py
+++ PZ_9/PZ_9.py
@@ -1,7 +1,9 @@
 # Организовать словарь на 10 англо-русских слов, обеспечивающий
 # "перевод" английского слова на русский.
 
-
 english_russian = {"apple": "яблоко", "cake": "торт", "false": "ложь", "true": "истина", "door": "дверь", "window": "окно", "keyboard": "клавиатура", "mouse": "мышь", "monitor": "монитор", "table": "стол"}
+a = english_russian.keys()
+print(a)
+b = input("Введите слово которое хотите перевести: ")
 
-print(english_russian["apple"])
+print(english_russian[b])
