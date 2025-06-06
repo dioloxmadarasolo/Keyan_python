408ff53078145870278158e91e1c79d2f429ee7b
dioloxmadarasolo <tkeyan@inbox.ru>
2025-06-06 16:37:34 +0300

    16 pz ez

--- PZ_16/PZ_16.py
+++ PZ_16/PZ_16.py
@@ -5,7 +5,6 @@ class Animal:
         self.type = type
 
     def animal(self):
-<<<<<<< HEAD
         print(f"Имя животного: {self.name}, вид: {self.type}")
 
 
@@ -16,11 +15,4 @@ cat = Animal("Кнопа", "Шотландская вислоухая кошка
 dog.animal()
 
 cat.animal()
-=======
-        print(f"Имя животного: {self.name}, вид:{self.type}")
 
-dog = Animal("Олег", "Чихахуа")
-cat = Animal("Кнопа", "Шотландская вислоухая кошка")
-dog.animal()
-cat.animal()
->>>>>>> origin/main
