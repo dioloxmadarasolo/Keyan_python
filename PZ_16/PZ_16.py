75d3d0c0cd98c95c715fc1b3799c7a48573d1022
dioloxmadarasolo <tkeyan@inbox.ru>
2025-06-06 16:37:02 +0300

    16 pz ez

--- PZ_16/PZ_16.py
+++ PZ_16/PZ_16.py
@@ -5,9 +5,13 @@ class Animal:
         self.type = type
 
     def animal(self):
-        print(f"Имя животного: {self.name}, вид:{self.type}")
+        print(f"Имя животного: {self.name}, вид: {self.type}")
+
 
 dog = Animal("Олег", "Чихахуа")
+
 cat = Animal("Кнопа", "Шотландская вислоухая кошка")
+
 dog.animal()
+
 cat.animal()
