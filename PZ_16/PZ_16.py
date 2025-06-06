cb8412390d790668afebacddbbadadac08108311
dioloxmadarasolo <tkeyan@inbox.ru>
2025-06-06 16:35:24 +0300

    16 pz ez

---
+++ PZ_16/PZ_16.py
@@ -0,0 +1,13 @@
+class Animal:
+
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type
+
+    def animal(self):
+        print(f"Имя животного: {self.name}, вид:{self.type}")
+
+dog = Animal("Олег", "Чихахуа")
+cat = Animal("Кнопа", "Шотландская вислоухая кошка")
+dog.animal()
+cat.animal()
